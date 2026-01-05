479422019ec0456ba5a9f38a68760119f8e94b42
congcq <aiskai2007@gmail.com>
2026-01-05 12:35:58 +0700

    Fix: keychain load failed due to missing allowed classes (Amethyst's commit)

--- Natives/authenticator/MicrosoftAuthenticator.m
+++ Natives/authenticator/MicrosoftAuthenticator.m
@@ -261,7 +261,12 @@ typedef void(^XSTSCallback)(NSString *xsts, NSString *uhs);
     CFTypeRef result = nil;
     OSStatus status = SecItemCopyMatching((__bridge CFDictionaryRef)dict, &result);
     if (status == errSecSuccess) {
-        return [NSKeyedUnarchiver unarchivedObjectOfClass:NSDictionary.class fromData:(__bridge NSData *)result error:nil];
+        NSError *error;
+        id result [NSKeyedUnarchiver unarchivedObjectOfClass:@[NSDictionary.class, NSString.class] fromData:(__bridge NSData *)result error:nil];
+        if (error) {
+            NSDebugLog(@"[MicrosoftAuthenicator] Failed to unarchive token data: %@", error);
+        }
+        return result;
     } else {
         return nil;
     }
